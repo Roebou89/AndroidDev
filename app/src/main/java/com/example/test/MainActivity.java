f92ad4ebe0fbeb78aa685f75c9b412af42585204
Rohan de Jongh <rohan.dejongh@4cgroup.co.za>
2018-12-03 11:47:49 +0200

    Initial Commit

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivit {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
